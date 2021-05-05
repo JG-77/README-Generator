e8ead004b9b37db84963c0f1a247e96c1b9738b2
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-05-04 18:53:08 -0700

    created Markdown return to generate README

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -12,10 +12,47 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} (Banner)
 
-  ##
+  ## Table of Contents
 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test](#test)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Description
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Test
+
+  ${data.test}
+
+  ## License
+
+  ${data.license} 
+
+  ## Questions
+
+  - GitHub Profile: https://github.com/${data.github}
+  
+  - Email: ${data.email}
 `; 
 } 
 
